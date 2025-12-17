ba8a64a468ff4fbbbb38e07be2abe865c1680f94
Mohanakrishnan-k <mohanakrishnanking@gmail.com>
2025-12-17 10:18:00 -0800

    bug fix

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>
+        This is a demo project created for learning purposes only.  
+        Not affiliated with Amazon or any real company.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
